463a92c1d4a09a48622a2aba4d929414a42316ff
unknown <taco.sauce.dev@gmail.com>
2022-12-24 13:23:07 -0700

    Added dev branch\n Added default branch

--- terraform/repo.tf
+++ terraform/repo.tf
@@ -15,6 +15,21 @@ resource "github_repository" "github" {
     }
   }
 }
+resource "github_branch" "github_main" {
+  repository = github_repository.github.name
+  branch     = "main"
+}
+resource "github_branch" "github_dev" {
+  repository = github_repository.github.name
+  branch     = "dev"
+}
+resource "github_branch_default" "github_default" {
+  depends_on = [
+    github_branch.github_dev
+  ]
+  repository = github_repository.github.name
+  branch     = github_branch.github_dev.branch
+}
 resource "github_repository" "terraform-account-wide" {
   name        = "terraform-account-wide"
   description = "For managing github with terraform."
@@ -31,4 +46,19 @@ resource "github_repository" "terraform-account-wide" {
       status = "enabled"
     }
   }
+}
+resource "github_branch" "terraform-account-wide-main" {
+  repository = github_repository.terraform-account-wide.name
+  branch     = "main"
+}
+resource "github_branch" "terraform-account-wide-dev" {
+  repository = github_repository.terraform-account-wide.name
+  branch     = "dev"
+}
+resource "github_branch_default" "terraform-account-wide_default" {
+  depends_on = [
+    github_branch.terraform-account-wide-dev
+  ]
+  repository = github_repository.terraform-account-wide.name
+  branch     = github_branch.terraform-account-wide-dev.branch
 }
